055c58d3d9be699b6238891da73dc84aaf7806a6
spethstation <114708141+spethstation@users.noreply.github.com>
2022-10-18 19:47:13 -0300

    Changed 5 day forecast to not include today

--- src/forecast2.js
+++ src/forecast2.js
@@ -41,7 +41,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
   let fiveDayForecast = response.data.daily;
   fiveDayForecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `
